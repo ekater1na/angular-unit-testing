7b7a1ca08d8f04a34f1be93246b9cbb8694a1f90
ekater1na <ekaterina_l@tut.by>
2022-02-08 15:49:06 +0300

    feat: add tests for a component

---
+++ src/app/heroes/heroes.component.spec.ts
@@ -0,0 +1,45 @@
+import {HeroesComponent} from "./heroes.component";
+import {of} from "rxjs";
+
+describe('HeroesComponent', () => {
+  let component: HeroesComponent;
+  let HEROES;
+  let mockHeroService;
+
+  beforeEach(() => {
+    HEROES = [
+      {id: 1, name: 'SpiderDude', strength: 8},
+      {id: 2, name: 'Wonderful Woman', strength: 24},
+      {id: 2, name: 'SuperDude', strength: 55}
+    ];
+
+    mockHeroService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero'])
+
+    component = new HeroesComponent(mockHeroService);
+  });
+
+  describe('delete', () => {
+
+    // mocking isolated code
+    it('should remove the indicated hero from the heroes list', () => {
+      mockHeroService.deleteHero.and.returnValue(of(true));
+      component.heroes = HEROES;
+
+      component.delete(HEROES[2]);
+
+      expect(component.heroes.length).toBe(2)
+    })
+
+
+    // testing interactions
+    it('should call deleteHero', () => {
+      mockHeroService.deleteHero.and.returnValue(of(true));
+      component.heroes = HEROES;
+
+      component.delete(HEROES[2]);
+
+      expect(mockHeroService.deleteHero).toHaveBeenCalledWith(HEROES[2]);
+    })
+  })
+
+})
